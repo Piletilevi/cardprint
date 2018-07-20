fb209e97d1f47bc4934929a56690d75615c74fea
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-07-20 21:33:20 +0300

    pilts'n'texts

--- cardprint.py
+++ cardprint.py
@@ -2,6 +2,8 @@ import sys
 import os
 import csv
 from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 
 def printCard(card):
@@ -11,8 +13,14 @@ def printCard(card):
     print(bg_fn, face_fn, out_fn)
     bg_img = Image.open(bg_fn)
     face_img = Image.open(face_fn)
-    face_offset = (35, 70)
+    face_offset = (100, 700)
     bg_img.paste(face_img, face_offset)
+    draw = ImageDraw.Draw(bg_img)
+    font = ImageFont.truetype("arial.ttf", 50)
+    font2 = ImageFont.truetype("courbd.ttf", 22)
+    draw.text((150, 550), card['eesnimi'], (0, 0, 0), font=font)
+    draw.text((200, 600), card['perenimi'], (0, 0, 0), font=font)
+    draw.text((150, 650), card['lisainfo'], (0, 0, 0), font=font2)
     bg_img.save(out_fn)
 
 
