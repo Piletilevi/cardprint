8cea5298e9d6623ad03addb2ce12c6932b4b991d
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-07-20 21:03:35 +0300

    sample data

--- cardprint.py
+++ cardprint.py
@@ -1,10 +1,22 @@
 import sys
+import os
 import csv
+from PIL import Image
 
 
 def printCard(card):
-    print(card)
+    bg_fn = os.path.join(datadir, card['taustafail'])
+    face_fn = os.path.join(datadir, card['pildifail'])
+    out_fn = os.path.join(datadir, card['kood']+'.png')
+    print(bg_fn, face_fn, out_fn)
+    bg_img = Image.open(bg_fn)
+    face_img = Image.open(face_fn)
+    face_offset = (35, 70)
+    bg_img.paste(face_img, face_offset)
+    bg_img.save(out_fn)
 
+
+datadir = os.path.dirname(sys.argv[1])
 
 with open(sys.argv[1], newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
